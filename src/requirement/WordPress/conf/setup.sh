ba1a8f19480c38d091bb0558467ef0284b441545
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-05-31 19:36:40 +0200

    feat: completed config wordpress

--- src/requirement/WordPress/conf/setup.sh
+++ src/requirement/WordPress/conf/setup.sh
@@ -2,12 +2,23 @@
 
 cd /srv/www/wordpress
 
+# wait to build MariaDB container
+sleep 10
+
 # set up database of wordpress
 wp config create \
     --dbname=${DB_NAME} \
     --dbuser=${DB_USER} \
     --dbpass=${DB_PASSWORD} \
     --dbhost=mariadb \
+    --allow-root
+
+wp core install \
+    --url=${WP_URL} \
+    --title=${WP_TITLE} \
+    --admin_user=${WP_ADMIN_USER} \
+    --admin_password=${WP_ADMIN_PASSWORD} \
+    --admin_email=${WP_ADMIN_EMAIL} \
     --allow-root
 
 exec "$@"
