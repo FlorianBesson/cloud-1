b43277326aee06becdc3b4dd63a8aec34e65acd6
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2026-06-08 17:08:55 +0200

    refa: check first

--- src/requirement/WordPress/conf/setup.sh
+++ src/requirement/WordPress/conf/setup.sh
@@ -1,27 +1,26 @@
 #! /bin/sh
 
 cd /srv/www/wordpress
-
 # wait to build MariaDB container
 sleep 10
-
-# set up database of wordpress
-# Ref: https://developer.wordpress.org/cli/commands/config/create/
-wp config create \
-    --dbname=${DB_NAME} \
-    --dbuser=${DB_USER} \
-    --dbpass=${DB_PASSWORD} \
-    --dbhost=mariadb \
-    --allow-root
-
-# create the wordpress tables in the database
-# Ref: https://developer.wordpress.org/cli/commands/core/install/
-wp core install \
-    --url=${WP_URL} \
-    --title=${WP_TITLE} \
-    --admin_user=${WP_ADMIN_USER} \
-    --admin_password=${WP_ADMIN_PASSWORD} \
-    --admin_email=${WP_ADMIN_EMAIL} \
-    --allow-root
+if [ ! -f /srv/www/wordpress/.setup_done ]; then
+    # set up database of wordpress
+    # Ref: https://developer.wordpress.org/cli/commands/config/create/
+    wp config create \
+        --dbname=${DB_NAME} \
+        --dbuser=${DB_USER} \
+        --dbpass=${DB_PASSWORD} \
+        --dbhost=mariadb \
+        --allow-root
+    # create the wordpress tables in the database
+    # Ref: https://developer.wordpress.org/cli/commands/core/install/
+    wp core install \
+        --url=${WP_URL} \
+        --title=${WP_TITLE} \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --allow-root
+fi
 
 exec "$@"
