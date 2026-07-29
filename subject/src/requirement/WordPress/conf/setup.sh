9e4812ad357b46ef4866a6e92bf53a82461fa2e5
florian <bessonflorian42@yahoo.fr>
2026-07-29 14:02:24 +0200

    delete post on wordpress

--- subject/src/requirement/WordPress/conf/setup.sh
+++ subject/src/requirement/WordPress/conf/setup.sh
@@ -36,12 +36,6 @@ if [ ! -f ${ROOT_WORDPRESS}.setup_done ]; then
         --post_status=publish \
         --path=${ROOT_WORDPRESS} \
         --allow-root
-    wp post create \
-        --post_title='new' \
-        --post_content='post' \
-        --post_status=publish \
-        --path=${ROOT_WORDPRESS} \
-        --allow-root
 fi
 
 exec "$@"
